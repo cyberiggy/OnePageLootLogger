2617b8e345327cc0116ef347de82dbbb1b8e8aee
Ron Woodbury <ronwoodbury@mac.com>
2021-11-24 19:53:32 -0700

    Initial Commit

---
+++ OnePageLootLogger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnePageLootLogger
+//
+//  Created by Ron Woodbury on 11/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnePageLootLogger/OnePageLootLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnePageLootLoggerApp.swift
+//  OnePageLootLogger
+//
+//  Created by Ron Woodbury on 11/24/21.
+//
+
+import SwiftUI
+
+@main
+struct OnePageLootLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
